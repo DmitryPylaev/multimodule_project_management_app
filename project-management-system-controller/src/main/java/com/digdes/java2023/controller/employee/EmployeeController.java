7a4ce9b24a683f0a13eacee6142ddb056d5fca03
PylaevDmitry <pilaevdmitry@yandex.ru>
2023-05-31 11:18:05 +0400

    log4j2 for controller

--- project-management-system-controller/src/main/java/com/digdes/java2023/controller/employee/EmployeeController.java
+++ project-management-system-controller/src/main/java/com/digdes/java2023/controller/employee/EmployeeController.java
@@ -7,6 +7,7 @@ import com.digdes.java2023.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/employee")
 @AllArgsConstructor
+@Log4j2
 @Tag(name = "EmployeeController", description = "Контроллер сотрудника")
 public class EmployeeController {
     EmployeeService employeeService;
@@ -22,24 +24,28 @@ public class EmployeeController {
     @Operation(summary = "Внесение сотрудника")
     @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto create(@RequestBody CreateEmployeeDto employeeDto) {
+        log.info("В методе контроллера Employee::create");
         return employeeService.create(employeeDto);
     }
 
     @Operation(summary = "Изменение существующей записи о сотруднике")
     @PostMapping(value = "/edit", consumes = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto edit(@RequestBody EditEmployeeDto employeeDto) {
+        log.info("В методе контроллера Employee::edit");
         return employeeService.edit(employeeDto);
     }
 
     @Operation(summary = "Получение профиля по идентефикатору")
     @GetMapping(value = "/get_by_id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto get(@PathVariable long id) {
+        log.info("В методе контроллера Employee::getById");
         return employeeService.get(id);
     }
 
     @Operation(summary = "Получение профиля по учетной записи")
     @GetMapping(value = "/get_by_account/{account}", produces = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto get(@PathVariable String account) {
+        log.info("В методе контроллера Employee::getByAccount");
         return employeeService.get(account);
     }
 
@@ -48,12 +54,14 @@ public class EmployeeController {
             " и только среди активных сотрудников")
     @GetMapping(value = "/find", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<EmployeeDto> find(@RequestParam String input) {
+        log.info("В методе контроллера Employee::find");
         return employeeService.find(input);
     }
 
     @Operation(summary = "Удаление сотрудника, сотрудник переводится в статус REMOVED")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto delete(@PathVariable long id) {
+        log.info("В методе контроллера Employee::delete");
         return employeeService.delete(id);
     }
 }
