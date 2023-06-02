4d641a5da64d5abc300cbb885bd51d7ce894a69a
PylaevDmitry <pilaevdmitry@yandex.ru>
2023-06-02 12:09:44 +0400

    add pageable method again, fix merge bug

--- project-management-system-repository/src/main/java/com/digdes/java2023/repository/EmployeeRepository.java
+++ project-management-system-repository/src/main/java/com/digdes/java2023/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.digdes.java2023.repository;
 
 import com.digdes.java2023.model.employee.Employee;
 import com.digdes.java2023.model.employee.EmployeeStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -19,4 +21,13 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
             String infixPatronymic,
             String infixAccount,
             String infixEmail);
+
+    Page<Employee> findByEmployeeStatusAndLastNameContainingOrNameContainingOrPatronymicContainingOrAccountContainingOrEmailContaining(
+            String employeeStatus,
+            String infixLastname,
+            String infixName,
+            String infixPatronymic,
+            String infixAccount,
+            String infixEmail,
+            Pageable pageable);
 }
