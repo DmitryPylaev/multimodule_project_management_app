c73ae24072c900564ce56de32051ae084a7bbc2e
PylaevDmitry <pilaevdmitry@yandex.ru>
2023-06-02 11:06:23 +0400

    reload to pageable EmployeeRepository method

--- project-management-system-repository/src/main/java/com/digdes/java2023/repository/EmployeeRepository.java
+++ project-management-system-repository/src/main/java/com/digdes/java2023/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.digdes.java2023.repository;
 
 import com.digdes.java2023.model.employee.Employee;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +13,7 @@ import java.util.Optional;
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByAccount(String account);
     Optional<Employee> findByUsername(String username);
+
     List<Employee> findByEmployeeStatusAndLastNameContainingOrNameContainingOrPatronymicContainingOrAccountContainingOrEmailContaining(
             String employeeStatus,
             String infixLastname,
@@ -18,4 +21,13 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
             String infixPatronymic,
             String infixAccount,
             String infixEmail);
+
+    Page<Employee> findByEmployeeStatusAndLastNameContainingOrNameContainingOrPatronymicContainingOrAccountContainingOrEmailContaining(
+            String employeeStatus,
+            String infixLastname,
+            String infixName,
+            String infixPatronymic,
+            String infixAccount,
+            String infixEmail,
+            Pageable pageable);
 }
