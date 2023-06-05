41dd42fa16c6a723aebeb78858873e94a9b43268
PylaevDmitry <pilaevdmitry@yandex.ru>
2023-06-05 14:56:16 +0400

    rabbitmq config and template

---
+++ project-management-system-controller/src/main/java/com/digdes/java2023/TestMailSender.java
@@ -0,0 +1,7 @@
+package com.digdes.java2023;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestMailSender {
+}
---
+++ project-management-system-controller/src/main/java/com/digdes/java2023/amqp/MessageConsumer.java
@@ -0,0 +1,24 @@
+package com.digdes.java2023.amqp;
+
+import com.digdes.java2023.TestMailSender;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MessageConsumer {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final TestMailSender testMailSender;
+
+    public MessageConsumer(RabbitTemplate rabbitTemplate, TestMailSender testMailSender) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.testMailSender = testMailSender;
+    }
+
+    @RabbitListener(queues = "${rabbitmq.queue}")
+    public void receiveMessage(Message message) {
+        System.out.println(message.toString());
+    }
+}
