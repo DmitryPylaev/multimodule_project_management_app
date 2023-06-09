dd9d5ec7f177bcd1a738471bcd06c71b861ce2c4
PylaevDmitry <pilaevdmitry@yandex.ru>
2023-06-09 18:21:13 +0400

    sout to log.info

--- project-management-system-controller/src/main/java/com/digdes/java2023/amqp/MessageConsumer.java
+++ project-management-system-controller/src/main/java/com/digdes/java2023/amqp/MessageConsumer.java
@@ -1,6 +1,7 @@
 package com.digdes.java2023.amqp;
 
 import com.digdes.java2023.mail.TestMailSender;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import javax.mail.MessagingException;
 
 @Configuration
+@Log4j2
 public class MessageConsumer {
 
     private final RabbitTemplate rabbitTemplate;
@@ -21,7 +23,7 @@ public class MessageConsumer {
 
     @RabbitListener(queues = "${rabbitmq.queue}")
     public void receiveMessage(Message message) throws MessagingException {
-        System.out.println(message.toString());
+        log.info(message.toString());
         testMailSender.send("lesson 8 test");
     }
 }
