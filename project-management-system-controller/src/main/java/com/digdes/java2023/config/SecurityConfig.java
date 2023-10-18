6a3488c52bf7f90dffabe5a22d8620d339403f48
Дмитрий Пылаев <pilaevdmitry@yandex.ru>
2023-10-18 14:27:51 +0400

    security config fix

--- project-management-system-controller/src/main/java/com/digdes/java2023/config/SecurityConfig.java
+++ project-management-system-controller/src/main/java/com/digdes/java2023/config/SecurityConfig.java
@@ -3,15 +3,18 @@ package com.digdes.java2023.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests()
-                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/**")
+                .requestMatchers("/swagger-ui/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -20,5 +23,10 @@ public class SecurityConfig {
                 .and()
                 .csrf().disable()
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**");
     }
 }
